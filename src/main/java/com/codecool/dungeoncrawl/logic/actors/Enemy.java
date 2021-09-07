4ac4879d21fa44257f70a829bd77d55b57cdd772
Michał Gawroński <79256098+GMichalCode@users.noreply.github.com>
2021-09-07 23:08:37 +0200

    .gitignore added

---
+++ src/main/java/com/codecool/dungeoncrawl/logic/actors/Enemy.java
@@ -0,0 +1,15 @@
+package com.codecool.dungeoncrawl.logic.actors;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Enemy extends Actor {
+
+    public Enemy(Cell cell) {
+        super(cell);
+    }
+
+    @Override
+    public String getTileName() {
+        return "enemy";
+    }
+}
