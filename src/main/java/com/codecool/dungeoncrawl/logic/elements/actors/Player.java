3c9b9f8c021bfa978be1f967e799931ba0f9eaad
marianna <marianna>
2021-09-14 22:38:43 +0200

    quick fix

--- src/main/java/com/codecool/dungeoncrawl/logic/elements/actors/Player.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/elements/actors/Player.java
@@ -22,12 +22,24 @@ public class Player extends Actor{
 	public String getTileName(){
 		return "player";
 	}
-	
-	public void move(int dx, int dy){
+
+	public void move(int dx, int dy) {
 		Cell nextCell = cell.getNeighbor(dx, dy);
-		cell.setActor(null);
-		nextCell.setActor(this);
-		cell = nextCell;
+		boolean isMovementOk = isMovementOk(nextCell);
+		if (isMovementOk) {
+			cell.setActor(null);
+			nextCell.setActor(this);
+			cell = nextCell;
+		}
+	}
+
+	public boolean isMovementOk(Cell nextCell) {
+		String monsterInCell = null;
+		if (nextCell.getActor() != null) {
+			monsterInCell = nextCell.getActor().getTileName();
+		}
+		String tileInCell = nextCell.getTileName();
+		return !(tileInCell.equals("wall") || "skeleton".equals(monsterInCell));
 	}
 	
 	public boolean isEnemy(int dx, int dy){
